3ca6db95dadbbd7bee329632e1206467f555dc55
krystianbrzezanski <krystianbrzezanski@gmail.com>
2026-03-30 17:53:56 +0200

    49

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,7 +1,7 @@
 <div class="flex items-center tracking-tighter">
     <span class="text-3xl mr-2"></span>
     
-    <span {{ $attributes->merge(['class' => 'font-black text-2xl']) }}>
-        <span class="text-violet-500">Warzywniak</span><span class="text-white">Spolem</span><span class="text-violet-500">.</span>
+    <span {{ $attributes->merge(['class' => 'font-black text-2xl uppercase italic']) }}>
+        <span class="text-emerald-600">Warzywniak</span><span class="text-black">Spolem</span><span class="text-emerald-600">.</span>
     </span>
 </div>
